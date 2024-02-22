e863f649949f90521980e9d7beadf2ecdca8f1ac
tyler543 <112919299+tyler543@users.noreply.github.com>
2024-02-22 13:47:46 -0500

    lab_assignment_6.c

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -9,12 +9,12 @@ int search(int numbers[], int low, int high, int value)
 	}
 	if(high >= low)
 	{
-		int mid = high/2;
+		int mid = low + (high - low)/2;
 		if(numbers[mid] == value)
-			return value;
+			return mid;
 		if(numbers[mid] > value)
-			return search (mid, low, mid - 1, value);
-		
+			return search (numbers, low, mid - 1, value);
+		return search(numbers, mid+1, high, value);
 	}
 	return -1;
 
